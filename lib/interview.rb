d6664573e78f3b926c4dc9af5808528bdf51bc36
Daniel Bogan <github.com@waferbaby.com>
2009-11-10 20:29:57 +1100

    Ditched the contents property for overview, hardware, software and dream_setup. Killed off the contents_with_wares method, and updated link_to_wares to support the new properties.

--- lib/interview.rb
+++ lib/interview.rb
@@ -15,43 +15,36 @@ class Interview
     property :person,       String
     property :summary,      String, :length => 100
     property :credits,      String, :length => 80
-    property :contents,     Text
+    property :overview,     Text
+    property :hardware,     Text
+    property :software,     Text
+    property :dream_setup,  Text    
     property :published_at, DateTime
     
     timestamps :at
     
     validates_is_unique :slug
-    validates_present :person, :summary, :contents
+    validates_present :person, :summary, :overview, :hardware, :software, :dream_setup
     
     has n, :wares, :through => Resource
     
     before :create, :link_to_wares
     before :update, :link_to_wares
     
-    def contents_with_wares
-        c = self.contents
-        
-        if self.wares.length > 0
-            c += "\r\n\r\n"
-            
-            self.wares.each do |ware|
-                c += "[#{ware.slug}]: #{ware.url} \"#{ware.description}\"\n"
-            end
-        end
-        
-        c
-    end
-    
     def link_to_wares
-        links = self.contents.scan(/\[([^\[\(\)]+)\]\[([a-z0-9\.\-]+)?\]/)
-        if links.length > 0
-            links.each do |link|
-                slug = link[1] ? link[1] : link[0].downcase
+        [overview, hardware, software, dream_setup].each do |content|
+            puts "Looking at #{content}..."
+            
+            links = content.scan(/\[([^\[\(\)]+)\]\[([a-z0-9\.\-]+)?\]/)
+            if links.length > 0
+                links.each do |link|
+                    slug = link[1] ? link[1] : link[0].downcase
                 
-                unless self.wares.first(:slug => slug)
-                    ware = Ware.first(:slug => slug)
-                    unless ware.nil?
-                        self.wares << ware
+                    unless self.wares.first(:slug => slug)
+                        ware = Ware.first(:slug => slug)
+                        unless ware.nil?
+                            self.wares << ware
+                        end
                     end
                 end
             end
