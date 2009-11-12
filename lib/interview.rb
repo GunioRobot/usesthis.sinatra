71dca205d7e506cffd8b69a918b8914a686dc546
Daniel Bogan <github.com@waferbaby.com>
2009-11-12 19:02:09 +1100

    Switched back to the single contents property. Added the contents_with_links method again. Changed link_to_wares to link_wares, wihch is now used as a validation method, which fills @unknown_wares with unknown... wares.

--- lib/interview.rb
+++ lib/interview.rb
@@ -4,50 +4,63 @@
 #
 
 require 'datamapper'
-require 'dm-is-paginated'
 
 class Interview
     include DataMapper::Resource
-    
-    is :paginated
     
     property :slug,         String, :key => true
     property :person,       String
     property :summary,      String, :length => 100
     property :credits,      String, :length => 80
-    property :overview,     Text
-    property :hardware,     Text
-    property :software,     Text
-    property :dream_setup,  Text    
+    property :contents,     Text
     property :published_at, DateTime
     
     timestamps :at
     
     validates_is_unique :slug
-    validates_present :person, :summary, :overview, :hardware, :software, :dream_setup
+    validates_present :person, :summary, :contents
+    validates_with_method :contents, :method => :link_wares
     
     has n, :wares, :through => Resource
     
-    before :create, :link_to_wares
-    before :update, :link_to_wares
+    before :create, :link_wares
+    before :update, :link_wares
     
-    def link_to_wares
-        [overview, hardware, software, dream_setup].each do |content|
-            links = content.scan(/\[([^\[\(\)]+)\]\[([a-z0-9\.\-]+)?\]/)
-            if links.length > 0
-                links.each do |link|
-                    slug = link[1] ? link[1] : link[0].downcase
-                
-                    unless self.wares.first(:slug => slug)
-                        ware = Ware.first(:slug => slug)
-                        unless ware.nil?
-                            self.wares << ware
-                        end
-                    end
+    attr_accessor :unknown_wares
+
+    def contents_with_links
+        result = attribute_get(:contents)
+        if self.wares.length > 0
+            result += "\r\n\r\n"
+
+            self.wares.each do |ware|
+                result += "[#{ware.slug}]: #{ware.url} \"#{ware.description}\"\n"
+            end
+        end
+
+        result
+    end
+    
+    def link_wares
+        @unknown_wares = []
+        
+        contents.scan(/\[([^\[\(\)]+)\]\[([a-z0-9\.\-]+)?\]/).each do |link|
+            slug = link[1] ? link[1] : link[0].downcase
+        
+            unless self.wares.first(:slug => slug)
+                ware = Ware.first(:slug => slug)
+                if ware.nil?
+                    @unknown_wares << slug
+                else
+                    self.wares << ware
                 end
             end
         end
-        
-        true
+
+        if @unknown_wares.empty?
+            true
+        else
+            [false, "Contents include unknown wares"]
+        end
     end
 end
