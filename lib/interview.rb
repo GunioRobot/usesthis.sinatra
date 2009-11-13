1a75a89ece9e29cfad48f94f6b37821a77986f44
Daniel Bogan <github.com@waferbaby.com>
2009-11-13 20:47:35 +1100

    Reworked the link_wares method to create a placeholder ware item if we don't find the slug.

--- lib/interview.rb
+++ lib/interview.rb
@@ -19,15 +19,12 @@ class Interview
     
     validates_is_unique :slug
     validates_present :person, :summary, :contents
-    validates_with_method :contents, :method => :link_wares
     
     has n, :wares, :through => Resource
     
     before :create, :link_wares
     before :update, :link_wares
     
-    attr_accessor :unknown_wares
-
     def contents_with_links
         result = attribute_get(:contents)
         if self.wares.length > 0
@@ -42,25 +39,22 @@ class Interview
     end
     
     def link_wares
-        @unknown_wares = []
-        
         contents.scan(/\[([^\[\(\)]+)\]\[([a-z0-9\.\-]+)?\]/).each do |link|
-            slug = link[1] ? link[1] : link[0].downcase
-        
-            unless self.wares.first(:slug => slug)
-                ware = Ware.first(:slug => slug)
-                if ware.nil?
-                    @unknown_wares << slug
-                else
-                    self.wares << ware
-                end
-            end
-        end
+            ware_slug = link[1] ? link[1] : link[0].downcase
 
-        if @unknown_wares.empty?
-            true
-        else
-            [false, "Contents include unknown wares"]
+            unless self.wares.first(:slug => ware_slug)
+                ware = Ware.first(:slug => ware_slug)
+                if ware.nil?
+                    ware = Ware.new(
+                        :slug           => ware_slug,
+                        :title          => ware_slug,
+                        :description    => '?',
+                        :url            => "/wares/#{ware_slug}/edit"
+                    )
+                end
+                
+                self.wares << ware
+            end
         end
     end
 end
