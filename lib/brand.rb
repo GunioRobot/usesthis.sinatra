63f73349c05c16820c40122957d33149504b34be
Daniel Bogan <github.com@waferbaby.com>
2010-08-30 23:34:26 -0700

    Initial import.

---
+++ lib/brand.rb
@@ -0,0 +1,17 @@
+require 'datamapper'
+
+class Brand
+    include DataMapper::Resource
+    
+    property :slug,         String, :key => true
+    property :title,        String
+    property :url,          String, :length => 250
+    property :description,  String, :length => 100
+    
+    timestamps :at
+    
+    validates_uniqueness_of :slug
+    validates_presence_of :title, :url, :description
+    
+    has n, :wares
+end
